386c1486f35ad5305e657a605b812f860c5ba0ec
Aetheruz <jamesromar.tuling@edu.sait.ca>
2025-04-19 17:41:38 -0600

    Updated Hero.jsx, and finished About.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,20 @@ import About from "./sections/About.jsx";
 import Projects from "./sections/Projects.jsx";
 import Clients from "./sections/Clients.jsx";
 import Contact from "./sections/Contact.jsx";
+import Footer from "./sections/Footer.jsx";
 
 const App = () => {
     return (
         <main className="w-full mx-auto">
-            <Navbar />
-            <Hero />
-            <About />
-            <Projects />
+            <Navbar/>
+            <Hero/>
+            <About/>
+            <Projects/>
             <Clients/>
 
             <Contact/>
+            <Footer/>
         </main>
-    )
-}
+    );
+};
 export default App
